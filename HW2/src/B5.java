3857ca82b32b8d2752d50f1efea76d4a8320b96d
Hung23020370 <donghung729@gmail.com>
2024-04-21 20:03:57 +0700

    BT

---
+++ HW2/src/B5.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+
+public class B5 {
+
+    public static void main(String[] args) {
+        Picture pic = new Picture(args[0]);
+        int width  = pic.width();
+        int height = pic.height();
+        pic.show();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width / 2; x++) {
+                Color c1 = pic.get(x, y);
+                Color c2 = pic.get(width - x - 1, y);
+                pic.set(x, y, c2);
+                pic.set(width - x - 1, y, c1);
+            }
+        }
+        pic.show();
+    }
+
+
+
+}
---
+++ HW2/src/TetrisGrid/clearrows.java
@@ -0,0 +1,46 @@
+public class clearrows {
+    public static void main(String[] args) {
+        boolean[][] grid = new boolean[][] {
+                {true, true, true},
+                {true, true, false},
+                {false, false, true},
+                {true, true, true},
+                {false, true, true},
+                {false, false, false},
+                {false, false, false},
+        };
+        clearRows(grid);
+    }
+
+    public static void clearRows(boolean[][] grid) {
+        int rows = grid.length; // Số hàng của mảng
+        int cols = grid[0].length; // Số cột của mảng
+        for (int i = 0; i < rows; i++) {
+            int cnt = 0;
+            for (int j = 0; j < cols; j++) {
+                if(grid[i][j]) cnt++;
+            }
+            if(cnt == cols) newGrid(grid,i,rows,cols);
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void newGrid(boolean[][] grid, int x, int rows, int cols) {
+        for (int i = x; i < rows - 1; i++) {
+            for (int j = 0; j < cols; j++) {
+                grid[i][j] = grid[i+1][j];
+            }
+        }
+        if(x == rows - 1) {
+            for (int j = 0; j < cols; j++) {
+                grid[x][j] = false;
+            }
+        }
+
+    }
+}
